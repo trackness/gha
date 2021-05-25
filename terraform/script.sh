81d9791032b42f75216b03bbe6c89948c4736925
trackness <40263851+trackness@users.noreply.github.com>
2021-05-25 17:00:47 +0100

    add stuff

--- terraform/script.sh
+++ terraform/script.sh
@@ -1,10 +1,24 @@
 cd "$WORKING_DIR" || exit
 
-destroy=$([ "$APPLY_DESTROY" == "true" ] && echo "-destroy")
+destroy=$([ "$APPLY_DESTROY" == "true" ] && echo " -destroy")
 backend_config=$([ -z "$BACKEND_CONFIG" ] || echo "-backend-config=$BACKEND_CONFIG")
 var_file=$([ -z "$VAR_FILE" ] || echo "-var-file=$VAR_FILE")
 
-terraform init -no-color -input=false "$backend_config"
+terraform init \
+  -no-color \
+  -input=false \
+  "$backend_config"
+
 terraform validate
-terraform plan -lock-timeout=60s -out=.terraform/"$PLAN_FILE" "$var_file" "$VARS"
-terraform apply "$destroy" -lock-timeout=60s -input=false -auto-approve .terraform/"$PLAN_FILE"
+
+terraform plan \
+  -lock-timeout=60s \
+  -out=.terraform/"$PLAN_FILE" \
+  "$var_file" \
+  "$VARS"
+
+terraform apply"$destroy" \
+  -lock-timeout=60s \
+  -input=false \
+  -auto-approve \
+  .terraform/"$PLAN_FILE"
